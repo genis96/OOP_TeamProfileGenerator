67d8bddda2d8337d6d390bdbd82e748af850ce17
Genis Bardales <genis96@yahoo.com>
2021-01-25 22:38:15 -0500

    not working yet..

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
+const startApp = require('./lib/index');
 const inquirer = require('inquirer');
-const fs = require('fs');
-// const Employee = require('./lib/employee');
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
-
-
-const employees = [];
-
-function initApp() {
-    startPage();
-    addTeamMember();
-}
-
-initApp();
-
+const app = new startApp();
+app.start();
 
