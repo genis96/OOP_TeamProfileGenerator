594d6ca22552c3c682678c1bf47a65aae81767dd
Genis Bardales <genis96@yahoo.com>
2021-04-04 16:38:24 -0400

    testing done

--- test/employee.test.js
+++ test/employee.test.js
@@ -6,8 +6,8 @@ test('employee case', () => {
 });
 test('set names', () => {
     const x = new Employee();
-    const name = 'Genis'
-    expect(x.name).toBe(name);
+    const testing = 'Genis'
+    expect(x.name).toBe(testing);
 });
 test('set email', () => {
     const x = new Employee('email', 1, testing);
@@ -19,3 +19,26 @@ test('set id', () => {
     const testing = 100;
     expect(x.id).toBe(testing);
 });
+
+test('get id, using getId()', () => {
+    const x = new Employee('number', testing);
+    const testing = 100;
+    expect(x.getId()).toBe(testing);
+});
+
+test('get email, using getEmail()', () => {
+    const x = new Employee('email', 1, testing);
+    const testing = 'genis@testing.com'
+    expect(x.getEmail()).toBe(testing);
+});
+test('get name, using getName()', () => {
+    const x = new Employee();
+    const testing = 'Genis'
+    expect(x.getName()).toBe(testing);
+});
+test('get role, using getRole()', () => {
+    const x = new Employee('email', 1, 'genis@testing.com' );
+    const testing = 'Employee';
+    expect(x.getRole()).toBe(testing);
+});
+
