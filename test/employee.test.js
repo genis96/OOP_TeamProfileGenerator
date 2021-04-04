b823864e1cbfbc4c0af2510674b6d9bfed756edf
Genis Bardales <genis96@yahoo.com>
2021-04-04 16:33:17 -0400

    set is done now work on get tests

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,52 +1,21 @@
 const Employee = require("../lib/employee");
 
-describe('Employee', () => {
-    it('start Employee', () => {
-        const x = new Employee();
-        expect(typeof(x)).toBe('object');
-    });
-    it('set name', () => {
-        const x = new Employee();
-        const name = 'GB'
-        expect(x.name).toBe(name);
-    });
-    it('set id', () => {
-        const x = new Employee('AB', testing);
-        const testing = 20;
-        expect(x.id).toBe(testing);
-    });
-    it('set email', () => {
-        const x = new Employee('GE', 1, testing);
-        const testing = 'ab@test.com';
-        expect(x.email).toBe(testing);
-    });
-    describe('getName', () => {
-        it('gets name via getName', () => {
-            const testing = 'El';
-            const x = new Employee(testing);
-            expect(x.getName()).toBe(testing);
-        })
-    })
-    describe('getId', () => {
-        it('gets id via getId', () => {
-            const testing = 20;
-            const x = new Employee('Ele', testing);
-            expect(x.getId()).toBe(testing);
-        })
-    })
-    describe('getEmail', () => {
-        it('gets email via getEmail', () => {
-            const testing = 'el@testing.com';
-            const x = new Employee('Ele', 1, testing);
-            expect(x.getEmail()).toBe(testing);
-        })
-    })
-    describe('getRole', () => {
-        it('gets role via getRole', () => {
-            const testing = 'el@testing.com';
-            const x = new Employee('Ge', 1, 'ge@test.com');
-            expect(x.getRole()).toBe(testing);
-        })
-    })
-})
-
+test('employee case', () => {
+    const e = new Employee();
+    expect(typeof x).toBe('object');
+});
+test('set names', () => {
+    const x = new Employee();
+    const name = 'Genis'
+    expect(x.name).toBe(name);
+});
+test('set email', () => {
+    const x = new Employee('email', 1, testing);
+    const testing = 'genis@testing.com'
+    expect(x.email).toBe(testing);
+});
+test('set id', () => {
+    const x = new Employee('number', testing);
+    const testing = 100;
+    expect(x.id).toBe(testing);
+});
